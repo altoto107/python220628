1d312db609275098b340dc38af8df1c8cb674ba9
altoto107 <altoto@naver.com>
2022-06-28 14:53:22 +0900

    업데이트 버전

--- Function4.py
+++ Function4.py
@@ -1,6 +1,7 @@
 #Function4.py
 
 #기본값이 있는 경우 
+from tkinter import Y
 from unittest import result
 
 
@@ -35,3 +36,28 @@ def union(*ar):
 print(union("HAM","SPAM"))
 print(union("HAM","SPAM","EGG"))
 
+
+#정의되지 않은 인자 처리 
+def userURIBuilder(server, port, **user):
+    strURL = "http://" + server + ":" + port + "/?"
+    for key in user.keys():
+        strURL += key +"=" +user[key]+ "&"
+    return strURL
+
+    
+
+#호출
+print(userURIBuilder("credu.com","80",id="kim", passwd="1234"))
+print(userURIBuilder("credu.com","80",id="kim",passwd="1234",
+      name="mike"))
+
+print(globals())
+
+
+#람다함수 정의 
+g = lambda x,y:x*y
+print(g(3,4))
+print(g(5,6))
+print((lambda x:x*x)(3))
+
+print(globals())

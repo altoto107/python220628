6aa208b25f90b0d0f4bf11f5b22c2065f5e521f3
altoto107 <altoto@naver.com>
2022-06-30 15:19:42 +0900

    클리앙 중고 장터 검색

--- web3.py
+++ web3.py
@@ -5,6 +5,8 @@ from turtle import tilt, title
 from bs4 import BeautifulSoup
 #웹서버에 요청
 import urllib.request
+#파일로 저장
+f = open("c:\\work\\webtoon.txt", "wt", encoding="utf-8")
 
 #페이징 처리를 하는 경우 , 정수를 문자로 바꾸는 str
 for i in range (1,6):
@@ -13,20 +15,14 @@ for i in range (1,6):
 	data = urllib.request.urlopen(url)
 	#검색이 용이한 객체 
 	soup = BeautifulSoup(data, "html.parser")
-	# <td class="title">
-	# 	<a href="/webtoon/detail">마음의 소리 50화 &lt;격렬한 나의 하루&gt;</a>
-	# </td>
-
 	cartoons = soup.find_all("td", class_="title")
-	#리스트 형태 객체 : 0, 1번
-	title = cartoons[0].find("a").text
-	link = cartoons[0].find("a")["href"]
-	print("갯수:{0}".format(len(cartoons)))
-	print(title)
-	print(link)
-
+	
 	for item in cartoons:
 		title = item.find("a").text
 		print(title.strip())
+        f.write 
+        
+        
 
+print("---웹 크롤링 종료---")
 

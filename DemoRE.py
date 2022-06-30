d86f43cb73ccb08dc9164c8abfc32d92d918540f
altoto107 <altoto@naver.com>
2022-06-30 11:18:40 +0900

    로그 파일 분석 주석

--- DemoRE.py
+++ DemoRE.py
@@ -15,3 +15,11 @@ print(bool(re.search("apple", "this is apple")))
 #search가 일반적인 검색
 print(bool(re.match("apple", "this is apple")))
 #match는 앞에만 보고 검색 끝 
+
+
+#연도를 찾는 경우 
+result = re.search("\d{4}", "올해는 2022년")
+print(result.group())
+
+result = re.search("\d{5}", "우리동네는 52300")
+print(result.group())

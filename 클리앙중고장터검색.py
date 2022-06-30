6aa5c9492b63b84e0e267a1ee60256e87dbe5a66
altoto107 <altoto@naver.com>
2022-06-30 15:28:52 +0900

    클리앙 크롤링

--- 클리앙중고장터검색.py
+++ 클리앙중고장터검색.py
@@ -7,6 +7,8 @@ import re
 #User-Agent를 조작하는 경우(아이폰에서 사용하는 사파리 브라우져의 헤더) 
 hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
 
+#파일로 저장
+f = open("c:\\work\\clien.txt", "wt", encoding="utf-8")
 for n in range(0,10):
         #클리앙의 중고장터 주소 
         data ='https://www.clien.net/service/board/sold?&od=T31&po=' + str(n)
@@ -14,8 +16,8 @@ for n in range(0,10):
         #웹브라우져 헤더 추가 
         req = urllib.request.Request(data, \
                                     headers = hdr)
-        req = urllib.request.Request(data)
         data = urllib.request.urlopen(req).read()
+        #한글이 깨지는 경우는 디코딩 (유니코드)
         page = data.decode('utf-8', 'ignore')
         soup = BeautifulSoup(page, 'html.parser')
         # <span class="subject_fixed" data-role="list-title-text" title="아이패드 프로 11인치 2세대 (2020) 셀룰러 256기가 스그">
@@ -25,11 +27,12 @@ for n in range(0,10):
 
         for item in list:
                 try:
-                       title = item.text
-                       print(title.strip())
-                        # if (re.search('아이폰', title)):
-                        #         print(title.strip())
-                        #         print('https://www.clien.net'  + item['href'])
+                        title = item.text
+                         #print(title.strip())
+                        if (re.search('맥북', title)):
+                                print(title.strip())
+                                f.write(title.strip() + "\n")                                
                 except:
                         pass
-        
+f.close 
+print("---웹크롤링 종료---")
